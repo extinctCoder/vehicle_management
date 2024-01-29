76f6756f2d9469d8fb61ff5b5e6f7871cd11f24e
darthVader <extinctCoder@outlook.com>
2024-01-29 08:16:32 +0000

    Jan 29, 2024, 2:16 PM

--- vehicle_management/vehicle_management/doctype/vehicle_price/vehicle_price.py
+++ vehicle_management/vehicle_management/doctype/vehicle_price/vehicle_price.py
@@ -7,12 +7,12 @@ from frappe.model.document import Document
 class VehiclePrice(Document):
 	def before_save(self):
 		self.sale_price = self.company_price+self.customer_price
-		# total_quantity = 0
-		# total_amount = 0
-		# for row in self.other_vehicle_items:
-		# 	total_quantity += row.quantity
-		# 	row.sale_price = row.company_price+row.customer_price
-		# 	total_amount += row.amount if not None else 0
-		# self.total_quantity = total_quantity
-		# self.total_amount = total_amount
-		# self.grand_total = self.sale_price + self.total_amount
+		total_quantity = 0
+		total_amount = 0
+		for row in self.other_vehicle_items:
+			total_quantity += row.quantity
+			row.amount = row.quantity*row.rate
+			total_amount += row.amount
+		self.total_quantity = total_quantity
+		self.total_amount = total_amount
+		self.grand_total = self.sale_price + self.total_amount
