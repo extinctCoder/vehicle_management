e9132aa14edcaa8ec8682e10e313dfdaa300ca00
darthVader <extinctCoder@outlook.com>
2024-01-29 12:10:40 +0600

    commit

---
+++ vehicle_management/vehicle_management/doctype/vehicle_grade/test_vehicle_grade.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, darthVader and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestVehicleGrade(FrappeTestCase):
+	pass
