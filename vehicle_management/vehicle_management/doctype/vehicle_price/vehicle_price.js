3a4ae61c2eec74ac38c9b40d586e7217a944c962
darthVader <extinctCoder@outlook.com>
2024-01-29 06:33:04 +0000

    Jan 29, 2024, 12:33 PM

---
+++ vehicle_management/vehicle_management/doctype/vehicle_price/vehicle_price.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, darthVader and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Vehicle Price', {
+	// refresh: function(frm) {
+
+	// }
+});
